6c045a49a95fe3e0817dbf23194d46cd06e456c4
mvhooren <contact@machiel.info>
2019-01-31 12:59:55 +0100

    Fixed GCC compile errors.

--- include/jitcat/ExpressionAssignmentHeaderImplementation.h
+++ include/jitcat/ExpressionAssignmentHeaderImplementation.h
@@ -76,14 +76,8 @@ inline bool ExpressionAssignment<ExpressionT>::assignInterpretedValue(CatRuntime
 		CatAssignableExpression* assignable = static_cast<CatAssignableExpression*>(expressionAST);
 		AssignableType assignableType = AssignableType::None;
 		std::any target = assignable->executeAssignable(runtimeContext, assignableType);
-		if constexpr (TypeTraits<ExpressionT>::isReflectableType())
-		{
-			ASTHelper::doAssignment(target, std::any(static_cast<Reflectable*>(value)), getExpectedCatType(), assignableType);
-		}
-		else
-		{
-			ASTHelper::doAssignment(target, std::any(value), getExpectedCatType(), assignableType);
-		}
+		std::any anyValue = TypeTraits<ExpressionT>::getCatValue(value);
+		ASTHelper::doAssignment(target, anyValue, getExpectedCatType(), assignableType);
 		return true;
 	}
 	return false;
