e34ce2df8923c69eaedf01b8707c4d0b7fa2e3ac
mvhooren <contact@machiel.info>
2019-04-24 16:48:33 +0200

    Added null check to assignments.

--- src/jitcat/ASTHelper.cpp
+++ src/jitcat/ASTHelper.cpp
@@ -55,11 +55,39 @@ void ASTHelper::doAssignment(std::any& target, std::any& source, const CatGeneri
 {
 	if (targetAssignableType == AssignableType::Pointer)
 	{
-		if		(type.isIntType())		*std::any_cast<int*>(target) = std::any_cast<int>(source);
-		else if (type.isFloatType())	*std::any_cast<float*>(target) = std::any_cast<float>(source);
-		else if (type.isBoolType())		*std::any_cast<bool*>(target) = std::any_cast<bool>(source);
-		else if (type.isStringType())	*std::any_cast<std::string*>(target) = std::any_cast<std::string>(source);
-		else if (type.isObjectType())	
+		if (type.isIntType())
+		{
+			int* intTarget = std::any_cast<int*>(target);
+			if (intTarget != nullptr)
+			{
+				*intTarget = std::any_cast<int>(source);
+			}
+		}
+		else if (type.isFloatType())
+		{
+			float* floatTarget = std::any_cast<float*>(target);
+			if (floatTarget != nullptr)
+			{
+				*floatTarget = std::any_cast<float>(source);
+			}
+		}
+		else if (type.isBoolType())
+		{
+			bool* boolTarget = std::any_cast<bool*>(target);
+			if (boolTarget != nullptr)
+			{
+				*boolTarget = std::any_cast<bool>(source);
+			}
+		}
+		else if (type.isStringType())
+		{
+			std::string* stringTarget = std::any_cast<std::string*>(target);
+			if (stringTarget != nullptr)
+			{
+				*stringTarget = std::any_cast<std::string>(source);
+			}
+		}
+		else if (type.isObjectType())
 		{
 			//Not supported for now. This would need to call operator= on the target object, not all objects will have implemented this.
 		}
@@ -67,12 +95,20 @@ void ASTHelper::doAssignment(std::any& target, std::any& source, const CatGeneri
 	}
 	else if (targetAssignableType == AssignableType::PointerPointer && type.isObjectType())
 	{
-		*std::any_cast<Reflectable**>(target)		= std::any_cast<Reflectable*>(source);
+		Reflectable** reflectableTarget = std::any_cast<Reflectable * *>(target);
+		if (reflectableTarget != nullptr)
+		{
+			*reflectableTarget = std::any_cast<Reflectable*>(source);
+		}
 		return;
 	}
-	else if (targetAssignableType == AssignableType::HandlePointer  && type.isObjectType())	
+	else if (targetAssignableType == AssignableType::HandlePointer && type.isObjectType())
 	{
-		*std::any_cast<ReflectableHandle*>(target)	= std::any_cast<Reflectable*>(source);
+		ReflectableHandle* handleTarget = std::any_cast<ReflectableHandle*>(target);
+		if (handleTarget != nullptr)
+		{
+			*handleTarget = std::any_cast<Reflectable*>(source);
+		}
 		return;
 	}
 
