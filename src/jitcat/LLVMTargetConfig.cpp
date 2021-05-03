f11b63c449a16230b2f67f47569cc7ea97ae33bc
Machiel van Hooren <contact@machiel.info>
2021-05-03 20:20:36 +0200

    Removed #pragma once in LLVMTargetConfig.cpp.

--- src/jitcat/LLVMTargetConfig.cpp
+++ src/jitcat/LLVMTargetConfig.cpp
@@ -5,8 +5,6 @@
   Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
 */
 
-#pragma once
-
 #include "jitcat/Configuration.h"
 #include "jitcat/LLVMTargetConfig.h"
 #include "jitcat/LLVMTypes.h"
